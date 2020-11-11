b853c57d583144436dedb624d540b4c67032ea62
mradulism <67101047+mradulism@users.noreply.github.com>
2020-11-11 12:07:33 +0530

    Update Exercise2.sql

--- RDBMS - Generics/Single-row Function/Exercise2.sql
+++ RDBMS - Generics/Single-row Function/Exercise2.sql
@@ -2,3 +2,7 @@
 
 alter session set current_schema=bank;
 SET heading OFF;
+SELECT trans_dt,substr(trans_desc,0,20) AS Trans_desc ,trans_amt 
+FROM bank_transaction 
+WHERE trans_dt>=TO_DATE( '01-Jun-2010 00:00:00 ',  'DD-Mon-YYYY HH24:MI:SS' )
+AND trans_dt<=TO_DATE( '30-Jun-2010 00:00:00 ',  'DD-Mon-YYYY HH24:MI:SS' );
